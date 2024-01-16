f96b4ac1189d1715a963502b23f48b334e4e6223
AnjasFedo <fedoaf98@gmail.com>
2024-01-16 07:56:11 +0700

    Create some Struct

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 	"strings"
 )
 
+type apiConfig struct{
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct{
+	Name string `json:"name"`
+	Main struct{
+		Kelvin float64 `json:"temp"`
+	}`json:"main"`
+}
+
 func main() {
 
 }
